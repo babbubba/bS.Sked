00a001b4e404718821a1b4b460dbe4578e242f50
Fabio Cavallari <fcavallari@bsoftsolutions.it>
2017-05-20 16:21:19 +0200

    Aggiunto SignalR alla Web Management Console

--- bS.Sked.WMC/Controllers/HomeController.cs
+++ bS.Sked.WMC/Controllers/HomeController.cs
@@ -13,18 +13,23 @@ namespace bS.Sked.WMC.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Dashboard()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
     }
 }
