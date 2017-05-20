54a615562643b25525d5edf62e33aa4cf2ae39aa
Fabio Cavallari <fcavallari@bsoftsolutions.it>
2017-05-20 15:40:59 +0200

    Aggiunto Servizio di Windows con Installer

---
+++ bS.Sked.WinService/WinServiceImpl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bS.Sked.WinService
+{
+    public partial class WinServiceImpl : ServiceBase
+    {
+        public WinServiceImpl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
