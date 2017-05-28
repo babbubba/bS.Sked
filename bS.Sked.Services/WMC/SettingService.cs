2328863eed7d490903c873937590a671edaa4cf2
Fabio Cavallari <fcavallari@bsoftsolutions.it>
2017-05-28 22:52:21 +0200

    Lavorando al servizio Settings per poter iniziare ad inserire un po di dati.

---
+++ bS.Sked.Services/WMC/SettingService.cs
@@ -0,0 +1,32 @@
+﻿using bS.Sked.Data.Interfaces;
+using bS.Sked.Model.Interfaces.Entities.Base;
+using bS.Sked.Models.Elements;
+using bS.Sked.Models.Interfaces.Elements;
+using bS.Sked.Services.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bS.Sked.Services.WMC
+{
+    public class SettingService : ServiceBase
+    {
+        private readonly IRepository<IPersisterEntity> repository;
+
+        public SettingService(IRepository<IPersisterEntity> repository)
+        {
+            this.repository = repository;
+        }
+
+        public ISmtpSettingModel[] SmtpSettings
+        {
+            get
+            {
+                return repository.GetQuery<ISmtpSettingModel>().ToArray();
+            }
+        }
+
+    }
+}
