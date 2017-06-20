b7eaf621d1578d4c894868230f904e0392227fcf
Fabio Cavallari <fcavallari@bsoftsolutions.it>
2017-06-20 22:57:41 +0200

    Sistemato layout bootstrap delle viste.

--- bS.Sked.ViewModel/Elements/SmtpSettingViewModel.cs
+++ bS.Sked.ViewModel/Elements/SmtpSettingViewModel.cs
@@ -3,6 +3,7 @@ using bS.Sked.ViewModel.Interfaces.Elements;
 using bS.Sked.ViewModel.Interfaces.Entities.Base;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,13 +12,19 @@ namespace bS.Sked.ViewModel.Elements
 {
     public class SmtpSettingViewModel : PersisterToggledDescribedEnityBaseVieModel,  ISmtpSettingViewModel
     {
-
+        [DisplayName("Is auth required")]
         public virtual bool IsAuthRequired { get; set; }
+        [DisplayName("Is default")]
         public virtual bool IsDefault { get; set; }
+        [DisplayName("Use SSL")]
         public virtual bool IsSslEnabled { get; set; }
+        [DisplayName("Smtp password")]
         public virtual string SmtpAccountPassword { get; set; }
+        [DisplayName("Smtp account")]
         public virtual string SmtpAccountUsername { get; set; }
+        [DisplayName("Smtp server address or ip")]
         public virtual string SmtpServerAddressOrIp { get; set; }
+        [DisplayName("Smtp server port")]
         public virtual int SmtpServerPort { get; set; }
     }
 }
@@ -32,6 +39,7 @@ namespace bS.Sked.ViewModel.Elements.Base
 
     public abstract class ToggledEntityBaseViewModel : IToggledEntityViewModel
     {
+        [DisplayName("Is active")]
         public virtual bool IsActive { get; set; }
     }
 
@@ -48,6 +56,7 @@ namespace bS.Sked.ViewModel.Elements.Base
             Id = Guid.Empty.ToString();
         }
         public virtual string Id { get; set; }
+        [DisplayName("Is active")]
         public virtual bool IsActive { get; set; }
         public virtual string Description { get; set; }
         public virtual string Name { get; set; }
