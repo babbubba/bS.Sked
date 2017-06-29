f7f58d53028713a52f426e847290e9add5e49566
Fabio Cavallari <fabio.cavallari@esqogito.com>
2017-06-29 12:22:00 +0200

    Sistemato mapping con caricamento profili... aggiunto xcopy post build per creare una cartella Output nella cartella della soluzione contenente il progetto WMC compilato e le extensions.

---
+++ bS.Sked.Services/ServicesMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using bs.Sked.Mapping;
+using bS.Sked.Models.Elements;
+using bS.Sked.Models.Interfaces.Elements;
+using bS.Sked.ViewModel.Elements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bS.Sked.Services
+{
+    public class ServicesMappingProfile : MapProfileBase
+    {
+        public ServicesMappingProfile()
+        {
+
+            CreateMap<SmtpSettingViewModel, SmtpSettingModel>();
+            CreateMap<SmtpSettingViewModel, ISmtpSettingModel>().ReverseMap();
+        }
+    }
+}
---
+++ bs.Sked.Mapping/MapProfileBase.cs
@@ -0,0 +1,14 @@
+﻿using bS.Sked.Mapping.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bs.Sked.Mapping
+{
+    public abstract class MapProfileBase : AutoMapper.Profile, IMapProfile
+    {
+
+    }
+}
